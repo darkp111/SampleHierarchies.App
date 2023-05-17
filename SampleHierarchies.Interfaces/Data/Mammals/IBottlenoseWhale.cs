2a3395ab72109754470ab668959d0bd8c1dc9a7a
Deeelllay <61250611+darkp111@users.noreply.github.com>
2023-05-17 19:05:54 +0200

    Update IBottlenoseWhale.cs

--- SampleHierarchies.Interfaces/Data/Mammals/IBottlenoseWhale.cs
+++ SampleHierarchies.Interfaces/Data/Mammals/IBottlenoseWhale.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +9,37 @@ namespace SampleHierarchies.Interfaces.Data.Mammals
     public interface IBottlenoseWhale : IMammal
     {
         #region Interface members
+        /// <summary>
+        /// Is a whale have echolocation
+        /// </summary>
         public bool IsEcholocation { get; set; }
+        /// <summary>
+        /// Description of echolocation
+        /// </summary>
         public string EcholocationDescription { get; set; }
+        /// <summary>
+        /// Is a whale toothed
+        /// </summary>
         public bool IsToothedWhale { get; set; }
+        /// <summary>
+        /// Description of whale toothed
+        /// </summary>
         public string ToothedWhaleDescription { get; set; }
+        /// <summary>
+        /// How much a whale lives
+        /// </summary>
         public int LongLifeSpan { get; set; }
+        /// <summary>
+        /// Is a whale sociable behavior
+        /// </summary>
         public bool IsSociableBehavior { get; set; }
+        /// <summary>
+        /// Description of whale social behavior
+        /// </summary>
         public string SociableBehaviorDescription { get; set; }
+        /// <summary>
+        /// Is a whale feed on squid
+        /// </summary>
         public string FeedsOnSquid { get; set; }
 
         #endregion
